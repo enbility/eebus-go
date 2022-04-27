17766833719fa065b864caee58e7d7dc9a952df3
Andreas Linde <mail@andreaslinde.de>
2022-04-27 14:35:13 +0200

    Move code from model into additions file

--- spine/model/commondatatypes.go
+++ spine/model/commondatatypes.go
@@ -1,11 +1,5 @@
 package model
 
-import (
-	"math"
-	"strconv"
-	"strings"
-)
-
 type ElementTagType struct{}
 
 type LabelType string
@@ -161,42 +155,6 @@ type ScaleType int8
 type ScaledNumberType struct {
 	Number *NumberType `json:"number,omitempty"`
 	Scale  *ScaleType  `json:"scale,omitempty"`
-}
-
-func (m ScaledNumberType) GetValue() float64 {
-	if m.Number == nil {
-		return 0
-	}
-	var scale float64 = 0
-	if m.Scale != nil {
-		scale = float64(*m.Scale)
-	}
-	return float64(*m.Number) * math.Pow(10, scale)
-}
-
-func NewScaledNumberType(value float64) *ScaledNumberType {
-	m := &ScaledNumberType{}
-
-	numberOfDecimals := 0
-	temp := strconv.FormatFloat(value, 'f', -1, 64)
-	index := strings.IndexByte(temp, '.')
-	if index > -1 {
-		numberOfDecimals = len(temp) - index - 1
-	}
-
-	if numberOfDecimals > 4 {
-		numberOfDecimals = 4
-	}
-
-	numberValue := NumberType(math.Trunc(value * math.Pow(10, float64(numberOfDecimals))))
-	m.Number = &numberValue
-
-	if numberValue != 0 {
-		scaleValue := ScaleType(-numberOfDecimals)
-		m.Scale = &scaleValue
-	}
-
-	return m
 }
 
 type ScaledNumberElementsType struct {
--- spine/model/commondatatypes_additions.go
+++ spine/model/commondatatypes_additions.go
@@ -2,7 +2,46 @@ package model
 
 import (
 	"fmt"
+	"math"
+	"strconv"
+	"strings"
 )
+
+func (m ScaledNumberType) GetValue() float64 {
+	if m.Number == nil {
+		return 0
+	}
+	var scale float64 = 0
+	if m.Scale != nil {
+		scale = float64(*m.Scale)
+	}
+	return float64(*m.Number) * math.Pow(10, scale)
+}
+
+func NewScaledNumberType(value float64) *ScaledNumberType {
+	m := &ScaledNumberType{}
+
+	numberOfDecimals := 0
+	temp := strconv.FormatFloat(value, 'f', -1, 64)
+	index := strings.IndexByte(temp, '.')
+	if index > -1 {
+		numberOfDecimals = len(temp) - index - 1
+	}
+
+	if numberOfDecimals > 4 {
+		numberOfDecimals = 4
+	}
+
+	numberValue := NumberType(math.Trunc(value * math.Pow(10, float64(numberOfDecimals))))
+	m.Number = &numberValue
+
+	if numberValue != 0 {
+		scaleValue := ScaleType(-numberOfDecimals)
+		m.Scale = &scaleValue
+	}
+
+	return m
+}
 
 func (r *FeatureAddressType) String() string {
 	if r == nil {
