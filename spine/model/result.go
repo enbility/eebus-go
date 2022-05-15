6e21dd60979164520639b1315f8e0dd14c83fd19
Andreas Linde <mail@andreaslinde.de>
2022-05-15 17:17:27 +0200

    Fix model typo

--- spine/model/result.go
+++ spine/model/result.go
@@ -12,7 +12,7 @@ const (
 	ErrorNumberTypeCommandNotSupported
 	ErrorNumberTypeCommandRejected
 	ErrorNumberTypeRestrictedFunctionExchangeCombinationNotSupported
-	ErrorNumberBindingIsNecessaryForThisCommand
+	ErrorNumberTypeBindingIsNecessaryForThisCommand
 )
 
 type ResultDataType struct {
