963b65c4fd9d60ce47cf559c920df0e20d1fc6b9
Andreas Linde <mail@andreaslinde.de>
2022-05-23 15:49:30 +0200

    Fix spine version

--- spine/const.go
+++ spine/const.go
@@ -2,4 +2,4 @@ package spine
 
 import "github.com/DerAndereAndi/eebus-go/spine/model"
 
-var SpecificationVersion model.SpecificationVersionType = "1.2.0"
+var SpecificationVersion model.SpecificationVersionType = "1.1.1"
