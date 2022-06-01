78c7df7ad3f8ae2ee62b73c54cf7555bc5fb39cf
taimo42 <60046002+taimo42@users.noreply.github.com>
2022-06-01 11:22:31 +0200

    integration tests for detaileddiscovery

--- go.mod
+++ go.mod
@@ -5,15 +5,16 @@ go 1.18
 require (
 	github.com/cenkalti/backoff v2.2.1+incompatible // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/google/go-cmp v0.5.8
 	github.com/miekg/dns v1.1.49 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/rickb777/plural v1.4.1 // indirect
-	github.com/stretchr/objx v0.1.0 // indirect
-	golang.org/x/mod v0.4.2 // indirect
-	golang.org/x/net v0.0.0-20220526153639-5463443f8c37 // indirect
+	github.com/stretchr/objx v0.4.0 // indirect
+	golang.org/x/mod v0.6.0-dev.0.20220106191415-9b9b3d81d5e3 // indirect
+	golang.org/x/net v0.0.0-20220531201128-c960675eff93 // indirect
 	golang.org/x/sys v0.0.0-20220520151302-bc2c85ada10a // indirect
-	golang.org/x/tools v0.1.6-0.20210726203631-07bc1bf47fb2 // indirect
-	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
+	golang.org/x/tools v0.1.10 // indirect
+	golang.org/x/xerrors v0.0.0-20220517211312-f3a8303e98df // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
 
---
+++ spine/integration_tests/nodemanagement_test.go
@@ -0,0 +1,104 @@
+package integrationtests
+
+import (
+	"encoding/json"
+	"os"
+	"testing"
+
+	"github.com/DerAndereAndi/eebus-go/spine"
+	"github.com/DerAndereAndi/eebus-go/spine/model"
+	"github.com/google/go-cmp/cmp"
+	"github.com/google/go-cmp/cmp/cmpopts"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/suite"
+)
+
+const (
+	detaileddiscoverydata_recv_read_file_path    = "./testdata/01_detaileddiscoverydata_recv_read.json"
+	detaileddiscoverydata_send_reply_file_prefix = "./testdata/01_detaileddiscoverydata_send_reply"
+)
+
+func TestNodeManagementSuite(t *testing.T) {
+	suite.Run(t, new(NodeManagementSuite))
+}
+
+type NodeManagementSuite struct {
+	suite.Suite
+	remote *spine.DeviceRemoteImpl
+	sut    *spine.DeviceLocalImpl
+	readC  chan []byte
+	writeC chan []byte
+}
+
+func (s *NodeManagementSuite) SetupSuite() {
+}
+
+func (s *NodeManagementSuite) BeforeTest(suiteName, testName string) {
+	s.sut = spine.NewDeviceLocalImpl("TestBrandName", "TestDeviceModel", "TestDeviceCode",
+		"TestSerialNumber", "TestDeviceAddress", model.DeviceTypeTypeChargingStation)
+
+	s.readC = make(chan []byte)
+	s.writeC = make(chan []byte)
+	s.remote = spine.NewDeviceRemoteImpl(s.sut, "", s.readC, s.writeC)
+}
+
+func (s *NodeManagementSuite) AfterTest(suiteName, testName string) {
+}
+
+func (s *NodeManagementSuite) TestDetailedDiscovery() {
+	// Act
+	s.readC <- loadFileData(s.T(), detaileddiscoverydata_recv_read_file_path)
+
+	// Assert
+	sendBytes := <-s.writeC
+	checkSentData(s.T(), sendBytes, detaileddiscoverydata_send_reply_file_prefix)
+}
+
+func (s *NodeManagementSuite) TestDetailedDiscoveryWithAcknowledge() {
+	// TODO
+}
+
+func loadFileData(t *testing.T, fileName string) []byte {
+	fileData, err := os.ReadFile(fileName)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	return fileData
+}
+
+func checkSentData(t *testing.T, sendBytes []byte, msgSendFilePrefix string) {
+	msgSendExpectedBytes, err := os.ReadFile(msgSendFilePrefix + "_expected.json")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	saveJsonToFile(t, sendBytes, msgSendFilePrefix+"_actual.json")
+	equal := jsonDatagramEqual(t, msgSendExpectedBytes, sendBytes)
+	assert.True(t, equal, "Assert equal failed for "+msgSendFilePrefix)
+}
+
+func jsonDatagramEqual(t *testing.T, expectedJson, actualJson []byte) bool {
+	var actualDatagram model.Datagram
+	if err := json.Unmarshal(actualJson, &actualDatagram); err != nil {
+		t.Fatal(err)
+	}
+	var expectedDatagram model.Datagram
+	if err := json.Unmarshal(expectedJson, &expectedDatagram); err != nil {
+		t.Fatal(err)
+	}
+
+	less := func(a, b model.FunctionPropertyType) bool { return string(*a.Function) < string(*b.Function) }
+	return cmp.Equal(expectedDatagram, actualDatagram, cmpopts.SortSlices(less))
+}
+
+func saveJsonToFile(t *testing.T, data json.RawMessage, fileName string) {
+	jsonIndent, err := json.MarshalIndent(data, "", "    ")
+	if err != nil {
+		t.Fatal(err)
+	}
+	err = os.WriteFile(fileName, jsonIndent, os.ModePerm)
+	if err != nil {
+		t.Fatal(err)
+	}
+}
