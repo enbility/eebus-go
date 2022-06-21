101db274fc23b53c8c17fc60a5b13e606eaa8226
taimo42 <60046002+taimo42@users.noreply.github.com>
2022-06-21 10:14:29 +0200

    Extract filter from CmdType

--- spine/model/commandframe_additions_test.go
+++ spine/model/commandframe_additions_test.go
@@ -24,3 +24,38 @@ func TestCmdType_Data(t *testing.T) {
 	assert.Equal(t, model.FunctionTypeNodeManagementDetailedDiscoveryData, *cmdData.Function)
 	assert.Equal(t, data, cmdData.Value)
 }
+
+func TestCmdType_ExtractFilter_NoFilter(t *testing.T) {
+	sut := &model.CmdType{
+		NodeManagementDetailedDiscoveryData: &model.NodeManagementDetailedDiscoveryDataType{},
+	}
+
+	// Act
+	filterPartial, filterDelete := sut.ExtractFilter()
+	assert.Nil(t, filterPartial)
+	assert.Nil(t, filterDelete)
+}
+
+func TestCmdType_ExtractFilter_FilterPartialDelete(t *testing.T) {
+
+	filterP := model.FilterType{
+		CmdControl: &model.CmdControlType{Partial: &model.ElementTagType{}},
+		NodeManagementDetailedDiscoveryDataSelectors: &model.NodeManagementDetailedDiscoveryDataSelectorsType{},
+	}
+	filterD := model.FilterType{
+		CmdControl: &model.CmdControlType{Delete: &model.ElementTagType{}},
+		NodeManagementDetailedDiscoveryDataSelectors: &model.NodeManagementDetailedDiscoveryDataSelectorsType{},
+	}
+
+	sut := &model.CmdType{
+		Filter:                              []model.FilterType{filterD, filterP},
+		NodeManagementDetailedDiscoveryData: &model.NodeManagementDetailedDiscoveryDataType{},
+	}
+
+	// Act
+	filterPartial, filterDelete := sut.ExtractFilter()
+	assert.NotNil(t, filterPartial)
+	assert.Equal(t, &filterP, filterPartial)
+	assert.NotNil(t, filterDelete)
+	assert.Equal(t, &filterD, filterDelete)
+}
