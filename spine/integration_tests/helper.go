5063a026bf7312a08e9f7a056cdf5eec7a3e4cce
taimo42 <60046002+taimo42@users.noreply.github.com>
2022-07-13 15:08:05 +0200

    moved some test test helper methods into own file

---
+++ spine/integration_tests/helper.go
@@ -0,0 +1,60 @@
+package integrationtests
+
+import (
+	"encoding/json"
+	"os"
+	"testing"
+
+	"github.com/DerAndereAndi/eebus-go/spine/model"
+	"github.com/google/go-cmp/cmp"
+	"github.com/google/go-cmp/cmp/cmpopts"
+	"github.com/stretchr/testify/assert"
+)
+
+func loadFileData(t *testing.T, fileName string) []byte {
+	fileData, err := os.ReadFile(fileName)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	return fileData
+}
+
+func checkSentData(t *testing.T, sendBytes []byte, msgSendFilePrefix string) {
+	msgSendExpectedBytes, err := os.ReadFile(msgSendFilePrefix + "_expected.json")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	msgSendActualFileName := msgSendFilePrefix + "_actual.json"
+	equal := jsonDatagramEqual(t, msgSendExpectedBytes, sendBytes)
+	if !equal {
+		saveJsonToFile(t, sendBytes, msgSendActualFileName)
+	}
+	assert.Truef(t, equal, "Assert equal failed! Check '%s' ", msgSendActualFileName)
+}
+
+func jsonDatagramEqual(t *testing.T, expectedJson, actualJson []byte) bool {
+	var actualDatagram model.Datagram
+	if err := json.Unmarshal(actualJson, &actualDatagram); err != nil {
+		t.Fatal(err)
+	}
+	var expectedDatagram model.Datagram
+	if err := json.Unmarshal(expectedJson, &expectedDatagram); err != nil {
+		t.Fatal(err)
+	}
+
+	less := func(a, b model.FunctionPropertyType) bool { return string(*a.Function) < string(*b.Function) }
+	return cmp.Equal(expectedDatagram, actualDatagram, cmpopts.SortSlices(less))
+}
+
+func saveJsonToFile(t *testing.T, data json.RawMessage, fileName string) {
+	jsonIndent, err := json.MarshalIndent(data, "", "    ")
+	if err != nil {
+		t.Fatal(err)
+	}
+	err = os.WriteFile(fileName, jsonIndent, os.ModePerm)
+	if err != nil {
+		t.Fatal(err)
+	}
+}
--- spine/integration_tests/nodemanagement_test.go
+++ spine/integration_tests/nodemanagement_test.go
@@ -1,14 +1,10 @@
 package integrationtests
 
 import (
-	"encoding/json"
-	"os"
 	"testing"
 
 	"github.com/DerAndereAndi/eebus-go/spine"
 	"github.com/DerAndereAndi/eebus-go/spine/model"
-	"github.com/google/go-cmp/cmp"
-	"github.com/google/go-cmp/cmp/cmpopts"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/suite"
 )
@@ -64,7 +60,7 @@ func (s *NodeManagementSuite) TestDetailedDiscovery_SendRead() {
 }
 
 func (s *NodeManagementSuite) TestDetailedDiscovery_SendReply() {
-	// irgnore detaileddiscoverydata_send_read
+	// ignore NodeManagementDetailedDiscoveryData read
 	<-s.writeC
 
 	// Act
@@ -76,7 +72,7 @@ func (s *NodeManagementSuite) TestDetailedDiscovery_SendReply() {
 }
 
 func (s *NodeManagementSuite) TestDetailedDiscovery_RecvReply() {
-	// irgnore detaileddiscoverydata_send_read
+	// ignore NodeManagementDetailedDiscoveryData read
 	<-s.writeC
 
 	// Act
@@ -129,11 +125,10 @@ func (s *NodeManagementSuite) TestDetailedDiscovery_RecvReply() {
 	assert.Equal(s.T(), model.FeatureTypeTypeDeviceDiagnosis, cemdd.Type())
 	assert.Equal(s.T(), model.RoleTypeClient, cemdd.Role())
 	assert.Equal(s.T(), 0, len(cemdd.Operations()))
-
 }
 
 func (s *NodeManagementSuite) TestDetailedDiscovery_SendReplyWithAcknowledge() {
-	// irgnore detaileddiscoverydata_send_read
+	// ignore NodeManagementDetailedDiscoveryData read
 	<-s.writeC
 
 	// Act
@@ -147,7 +142,7 @@ func (s *NodeManagementSuite) TestDetailedDiscovery_SendReplyWithAcknowledge() {
 }
 
 func (s *NodeManagementSuite) TestSubscriptionRequestCall_BeforeDetailedDiscovery() {
-	// irgnore detaileddiscoverydata_send_read
+	// ignore NodeManagementDetailedDiscoveryData read
 	<-s.writeC
 
 	// Act
@@ -165,7 +160,7 @@ func (s *NodeManagementSuite) TestSubscriptionRequestCall_BeforeDetailedDiscover
 }
 
 func (s *NodeManagementSuite) TestDestinationList_SendReply() {
-	// irgnore detaileddiscoverydata_send_read
+	// ignore NodeManagementDetailedDiscoveryData read
 	<-s.writeC
 
 	// Act
@@ -174,52 +169,4 @@ func (s *NodeManagementSuite) TestDestinationList_SendReply() {
 	// Assert
 	sendBytes := <-s.writeC
 	checkSentData(s.T(), sendBytes, destinationListData_send_reply_file_prefix)
-}
-
-func loadFileData(t *testing.T, fileName string) []byte {
-	fileData, err := os.ReadFile(fileName)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	return fileData
-}
-
-func checkSentData(t *testing.T, sendBytes []byte, msgSendFilePrefix string) {
-	msgSendExpectedBytes, err := os.ReadFile(msgSendFilePrefix + "_expected.json")
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	msgSendActualFileName := msgSendFilePrefix + "_actual.json"
-	equal := jsonDatagramEqual(t, msgSendExpectedBytes, sendBytes)
-	if !equal {
-		saveJsonToFile(t, sendBytes, msgSendActualFileName)
-	}
-	assert.Truef(t, equal, "Assert equal failed! Check '%s' ", msgSendActualFileName)
-}
-
-func jsonDatagramEqual(t *testing.T, expectedJson, actualJson []byte) bool {
-	var actualDatagram model.Datagram
-	if err := json.Unmarshal(actualJson, &actualDatagram); err != nil {
-		t.Fatal(err)
-	}
-	var expectedDatagram model.Datagram
-	if err := json.Unmarshal(expectedJson, &expectedDatagram); err != nil {
-		t.Fatal(err)
-	}
-
-	less := func(a, b model.FunctionPropertyType) bool { return string(*a.Function) < string(*b.Function) }
-	return cmp.Equal(expectedDatagram, actualDatagram, cmpopts.SortSlices(less))
-}
-
-func saveJsonToFile(t *testing.T, data json.RawMessage, fileName string) {
-	jsonIndent, err := json.MarshalIndent(data, "", "    ")
-	if err != nil {
-		t.Fatal(err)
-	}
-	err = os.WriteFile(fileName, jsonIndent, os.ModePerm)
-	if err != nil {
-		t.Fatal(err)
-	}
 }
