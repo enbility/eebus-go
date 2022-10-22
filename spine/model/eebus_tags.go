7eff66f3117d70b130676cda9f4ed17e75d2a71e
Andreas Linde <mail@andreaslinde.de>
2022-10-22 12:54:53 +0200

    Ignore if no eebus tag is added

--- spine/model/eebus_tags.go
+++ spine/model/eebus_tags.go
@@ -15,6 +15,9 @@ const EEBusTagName string = "eebus"
 func EEBusTags(field reflect.StructField) map[EEBusTag]string {
 	result := make(map[EEBusTag]string)
 	tags := field.Tag.Get(EEBusTagName)
+	if len(tags) == 0 {
+		return result
+	}
 	for _, tag := range strings.Split(tags, ";") {
 		pair := strings.Split(tag, ":")
 		if len(pair) != 2 {
