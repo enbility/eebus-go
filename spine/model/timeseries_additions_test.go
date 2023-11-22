f2646ea45ad7b2c14dd2f4661c10791ad1aef930
Andreas Linde <mail@andreaslinde.de>
2023-11-22 15:44:28 +0100

    Add a more complex timeseries update test

--- spine/model/timeseries_additions_test.go
+++ spine/model/timeseries_additions_test.go
@@ -58,6 +58,111 @@ func TestTimeSeriesListDataType_Update(t *testing.T) {
 	assert.Equal(t, 1, int(*item2.TimeSeriesSlot[0].TimeSeriesSlotId))
 }
 
+func TestTimeSeriesListDataType_Update_02(t *testing.T) {
+	sut := model.TimeSeriesListDataType{
+		TimeSeriesData: []model.TimeSeriesDataType{
+			{
+				TimeSeriesId: util.Ptr(model.TimeSeriesIdType(1)),
+				TimePeriod: &model.TimePeriodType{
+					StartTime: util.Ptr(model.AbsoluteOrRelativeTimeType("PT0S")),
+					EndTime:   util.Ptr(model.AbsoluteOrRelativeTimeType("P6D")),
+				},
+				TimeSeriesSlot: []model.TimeSeriesSlotType{
+					{
+						TimeSeriesSlotId: util.Ptr(model.TimeSeriesSlotIdType(0)),
+						TimePeriod: &model.TimePeriodType{
+							StartTime: util.Ptr(model.AbsoluteOrRelativeTimeType("PT0S")),
+							EndTime:   util.Ptr(model.AbsoluteOrRelativeTimeType("P6D")),
+						},
+						MaxValue: model.NewScaledNumberType(10000),
+					},
+				},
+			},
+			{
+				TimeSeriesId: util.Ptr(model.TimeSeriesIdType(2)),
+				TimePeriod: &model.TimePeriodType{
+					StartTime: util.Ptr(model.AbsoluteOrRelativeTimeType("PT0S")),
+				},
+				TimeSeriesSlot: []model.TimeSeriesSlotType{
+					{
+						TimeSeriesSlotId: util.Ptr(model.TimeSeriesSlotIdType(0)),
+						Duration:         util.Ptr(model.DurationType("P1DT6H46M33S")),
+						MaxValue:         model.NewScaledNumberType(0),
+					},
+					{
+						TimeSeriesSlotId: util.Ptr(model.TimeSeriesSlotIdType(1)),
+						Duration:         util.Ptr(model.DurationType("PT7H37M53S")),
+						MaxValue:         model.NewScaledNumberType(4410),
+					},
+					{
+						TimeSeriesSlotId: util.Ptr(model.TimeSeriesSlotIdType(2)),
+						Duration:         util.Ptr(model.DurationType("PT38M")),
+						MaxValue:         model.NewScaledNumberType(0),
+					},
+					{
+						TimeSeriesSlotId: util.Ptr(model.TimeSeriesSlotIdType(3)),
+						Duration:         util.Ptr(model.DurationType("PT32M")),
+						MaxValue:         model.NewScaledNumberType(4410),
+					},
+					{
+						TimeSeriesSlotId: util.Ptr(model.TimeSeriesSlotIdType(4)),
+						Duration:         util.Ptr(model.DurationType("P1D")),
+						MaxValue:         model.NewScaledNumberType(0),
+					},
+				},
+			},
+			{
+				TimeSeriesId: util.Ptr(model.TimeSeriesIdType(3)),
+				TimePeriod: &model.TimePeriodType{
+					StartTime: util.Ptr(model.AbsoluteOrRelativeTimeType("PT0S")),
+				},
+				TimeSeriesSlot: []model.TimeSeriesSlotType{
+					{
+						TimeSeriesSlotId: util.Ptr(model.TimeSeriesSlotIdType(1)),
+						Duration:         util.Ptr(model.DurationType("P1DT15H24M57S")),
+						Value:            model.NewScaledNumberType(44229),
+						MaxValue:         model.NewScaledNumberType(49629),
+					},
+				},
+			},
+		},
+	}
+
+	newData := model.TimeSeriesListDataType{
+		TimeSeriesData: []model.TimeSeriesDataType{
+			{
+				TimeSeriesId: util.Ptr(model.TimeSeriesIdType(3)),
+				TimePeriod: &model.TimePeriodType{
+					StartTime: util.Ptr(model.AbsoluteOrRelativeTimeType("PT0S")),
+				},
+				TimeSeriesSlot: []model.TimeSeriesSlotType{
+					{
+						TimeSeriesSlotId: util.Ptr(model.TimeSeriesSlotIdType(1)),
+						Duration:         util.Ptr(model.DurationType("P1DT15H16M50S")),
+						Value:            model.NewScaledNumberType(11539),
+						MaxValue:         model.NewScaledNumberType(49629),
+					},
+				},
+			},
+		},
+	}
+
+	// Act
+	sut.UpdateList(&newData, model.NewFilterTypePartial(), nil)
+
+	data := sut.TimeSeriesData
+	// check the non changing items
+	assert.Equal(t, 3, len(data))
+	item1 := data[0]
+	assert.Equal(t, 1, int(*item1.TimeSeriesId))
+	assert.Equal(t, 0, int(*item1.TimeSeriesSlot[0].TimeSeriesSlotId))
+	// check properties of updated item
+	item2 := data[2]
+	assert.Equal(t, 3, int(*item2.TimeSeriesId))
+	assert.Equal(t, 1, int(*item2.TimeSeriesSlot[0].TimeSeriesSlotId))
+	assert.Equal(t, 11539, int(item2.TimeSeriesSlot[0].Value.GetValue()))
+}
+
 func TestTimeSeriesDescriptionListDataType_Update(t *testing.T) {
 	sut := model.TimeSeriesDescriptionListDataType{
 		TimeSeriesDescriptionData: []model.TimeSeriesDescriptionDataType{
