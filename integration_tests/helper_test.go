ae8323308b61e10e61e4bcddf16d4a58c0bc2954
Andreas Linde <mail@andreaslinde.de>
2024-01-01 14:38:36 +0100

    Fixes for previous commit

--- integration_tests/helper_test.go
+++ integration_tests/helper_test.go
@@ -10,14 +10,11 @@ import (
 
 	"github.com/enbility/eebus-go/spine"
 	"github.com/enbility/eebus-go/spine/model"
-	"github.com/google/go-cmp/cmp"
-	"github.com/google/go-cmp/cmp/cmpopts"
-	"github.com/stretchr/testify/assert"
 )
 
 const (
-	wallbox_detaileddiscoverydata_recv_reply_file_path  = "./testdata/wallbox_detaileddiscoverydata_recv_reply.json"
-	wallbox_detaileddiscoverydata_recv_notify_file_path = "./testdata/wallbox_detaileddiscoverydata_recv_notify.json"
+	wallbox_detaileddiscoverydata_recv_reply_file_path  = "../spine/testdata/wallbox_detaileddiscoverydata_recv_reply.json"
+	wallbox_detaileddiscoverydata_recv_notify_file_path = "../spine/testdata/wallbox_detaileddiscoverydata_recv_notify.json"
 )
 
 type WriteMessageHandler struct {
@@ -131,45 +128,6 @@ func loadFileData(t *testing.T, fileName string) []byte {
 	}
 
 	return fileData
-}
-
-func checkSentData(t *testing.T, sendBytes []byte, msgSendFilePrefix string) {
-	msgSendExpectedBytes, err := os.ReadFile(msgSendFilePrefix + "_expected.json")
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	msgSendActualFileName := msgSendFilePrefix + "_actual.json"
-	equal := jsonDatagramEqual(t, msgSendExpectedBytes, sendBytes)
-	if !equal {
-		saveJsonToFile(t, sendBytes, msgSendActualFileName)
-	}
-	assert.Truef(t, equal, "Assert equal failed! Check '%s' ", msgSendActualFileName)
-}
-
-func jsonDatagramEqual(t *testing.T, expectedJson, actualJson []byte) bool {
-	var actualDatagram model.Datagram
-	if err := json.Unmarshal(actualJson, &actualDatagram); err != nil {
-		t.Fatal(err)
-	}
-	var expectedDatagram model.Datagram
-	if err := json.Unmarshal(expectedJson, &expectedDatagram); err != nil {
-		t.Fatal(err)
-	}
-
-	less := func(a, b model.FunctionPropertyType) bool { return string(*a.Function) < string(*b.Function) }
-	return cmp.Equal(expectedDatagram, actualDatagram, cmpopts.SortSlices(less))
-}
-
-func saveJsonToFile(t *testing.T, data json.RawMessage, fileName string) {
-	jsonIndent, err := json.MarshalIndent(data, "", "    ")
-	if err != nil {
-		t.Fatal(err)
-	}
-	err = os.WriteFile(fileName, jsonIndent, os.ModePerm)
-	if err != nil {
-		t.Fatal(err)
-	}
 }
 
 func waitForAck(t *testing.T, msgCounterReference *model.MsgCounterType, writeHandler *WriteMessageHandler) {
