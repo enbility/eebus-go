0333078562a11e1b937769ebb318a80be75d104d
Andreas Linde <mail@andreaslinde.de>
2024-02-21 12:36:56 +0100

    Add feature tests

---
+++ features/feature_test.go
@@ -0,0 +1,110 @@
+package features_test
+
+import (
+	"testing"
+
+	"github.com/enbility/eebus-go/features"
+	shipapi "github.com/enbility/ship-go/api"
+	spineapi "github.com/enbility/spine-go/api"
+	"github.com/enbility/spine-go/model"
+	"github.com/enbility/spine-go/spine"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/suite"
+)
+
+func TestFeatureSuite(t *testing.T) {
+	suite.Run(t, new(FeatureSuite))
+}
+
+type FeatureSuite struct {
+	suite.Suite
+
+	localEntity  spineapi.EntityLocalInterface
+	remoteEntity spineapi.EntityRemoteInterface
+
+	testFeature *features.Feature
+	sentMessage []byte
+}
+
+var _ shipapi.ShipConnectionDataWriterInterface = (*FeatureSuite)(nil)
+
+func (s *FeatureSuite) WriteShipMessageWithPayload(message []byte) {
+	s.sentMessage = message
+}
+
+func (s *FeatureSuite) BeforeTest(suiteName, testName string) {
+	s.localEntity, s.remoteEntity = setupFeatures(
+		s.T(),
+		s,
+		[]featureFunctions{
+			{
+				featureType: model.FeatureTypeTypeAlarm,
+				functions: []model.FunctionType{
+					model.FunctionTypeAlarmListData,
+				},
+			},
+		},
+	)
+
+	var err error
+	s.testFeature, err = features.NewFeature(model.FeatureTypeTypeAlarm, model.RoleTypeClient, model.RoleTypeServer, s.localEntity, s.remoteEntity)
+	assert.Nil(s.T(), err)
+	assert.NotNil(s.T(), s.testFeature)
+}
+
+func (s *FeatureSuite) Test_NewFeature() {
+	newFeature, err := features.NewFeature(model.FeatureTypeTypeBill, model.RoleTypeClient, model.RoleTypeServer, nil, s.remoteEntity)
+	assert.NotNil(s.T(), err)
+	assert.Nil(s.T(), newFeature)
+
+	newFeature, err = features.NewFeature(model.FeatureTypeTypeBill, model.RoleTypeClient, model.RoleTypeServer, s.localEntity, nil)
+	assert.NotNil(s.T(), err)
+	assert.Nil(s.T(), newFeature)
+
+	newFeature, err = features.NewFeature(model.FeatureTypeTypeBill, model.RoleTypeClient, model.RoleTypeServer, s.localEntity, s.remoteEntity)
+	assert.NotNil(s.T(), err)
+	assert.NotNil(s.T(), newFeature)
+
+	f := spine.NewFeatureLocal(1, s.localEntity, model.FeatureTypeTypeBill, model.RoleTypeClient)
+	s.localEntity.AddFeature(f)
+
+	newFeature, err = features.NewFeature(model.FeatureTypeTypeBill, model.RoleTypeClient, model.RoleTypeServer, s.localEntity, s.remoteEntity)
+	assert.NotNil(s.T(), err)
+	assert.NotNil(s.T(), newFeature)
+}
+
+func (s *FeatureSuite) Test_Subscription() {
+	subscription := s.testFeature.HasSubscription()
+	assert.Equal(s.T(), false, subscription)
+
+	counter, err := s.testFeature.Subscribe()
+	assert.Nil(s.T(), err)
+	assert.NotNil(s.T(), counter)
+
+	subscription = s.testFeature.HasSubscription()
+	assert.Equal(s.T(), true, subscription)
+
+	counter, err = s.testFeature.Subscribe()
+	assert.NotNil(s.T(), counter)
+	assert.Nil(s.T(), err)
+}
+
+func (s *FeatureSuite) Test_Binding() {
+	binding := s.testFeature.HasBinding()
+	assert.Equal(s.T(), false, binding)
+
+	counter, err := s.testFeature.Bind()
+	assert.Nil(s.T(), err)
+	assert.NotNil(s.T(), counter)
+
+	binding = s.testFeature.HasBinding()
+	assert.Equal(s.T(), true, binding)
+
+	counter, err = s.testFeature.Bind()
+	assert.NotNil(s.T(), counter)
+	assert.Nil(s.T(), err)
+}
+
+func (s *FeatureSuite) Test_ResultCallback() {
+	s.testFeature.AddResultCallback(10, func(msg spineapi.ResultMessage) {})
+}
