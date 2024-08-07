dc40c82cfe55b0ca6a6f00f926327d7046cc708a
Andreas Linde <mail@andreaslinde.de>
2024-08-07 08:31:33 +0200

    OPEV: Add support for Bender missing required data
    The Bender EVSE controller reports premitted value set data only for 1 phase (maybe detects if an EV is 1 phase only, or if the EVSE is connected 1 phase only), but has parameters for all possible phases.
    
    The OPEV spec 1.0.1.b says at page 30 “At least one set of permitted values SHALL be stated.” which it does not follow.
    
    Also LoadControlLimitListData only contains by default the limitIds, but none of the other mandatory fields.

--- usecases/internal/loadcontrol.go
+++ usecases/internal/loadcontrol.go
@@ -81,7 +81,11 @@ func LoadControlLimits(
 			}
 			_, dataMax, _, err := evElectricalConnection.GetPermittedValueDataForFilter(filter)
 			if err != nil {
-				return
+				// the device provides no limits for this phase (e.g. Bender 1 Phase)
+				// according to OpEV Spec 1.0.1b, page 30: "At least one set of permitted values SHALL be stated."
+				// which is not the case for all elements for this EVSE setup
+				// but we have to ignore this case and continue with the next phase
+				continue
 			}
 
 			limitValue = dataMax
