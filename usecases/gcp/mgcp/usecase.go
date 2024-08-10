d7e66b008f036dacd87aae85c3000d727bf1e633
Nils Prommersberger <nils.prommersberger@icloud.com>
2024-08-10 16:42:48 +0200

    Added descriptions defined in MGCP-V1.0.0

--- usecases/gcp/mgcp/usecase.go
+++ usecases/gcp/mgcp/usecase.go
@@ -4,7 +4,7 @@ import (
 	"github.com/enbility/eebus-go/api"
 	"github.com/enbility/eebus-go/features/server"
 	ucapi "github.com/enbility/eebus-go/usecases/api"
-	usecase "github.com/enbility/eebus-go/usecases/usecase"
+	"github.com/enbility/eebus-go/usecases/usecase"
 	spineapi "github.com/enbility/spine-go/api"
 	"github.com/enbility/spine-go/model"
 	"github.com/enbility/spine-go/spine"
@@ -241,12 +241,20 @@ func (e *MGCP) AddFeatures() {
 		ScopeType:       util.Ptr(model.ScopeTypeTypeACFrequency),
 	})
 
+	if e.idM1 == nil || e.idM2 == nil || e.idM3 == nil || e.idM41 == nil || e.idM42 == nil || e.idM43 == nil ||
+		e.idM51 == nil || e.idM52 == nil || e.idM53 == nil || e.idM54 == nil || e.idM55 == nil || e.idM56 == nil || e.idM6 == nil {
+		panic("failed to add measurement description")
+
+	}
+
 	electricalConnection, err := server.NewElectricalConnection(e.LocalEntity)
 	if err != nil {
 		panic(err)
 	}
 
+	idEc1 := model.ElectricalConnectionIdType(0)
 	err = electricalConnection.AddDescription(model.ElectricalConnectionDescriptionDataType{
+		ElectricalConnectionId:  util.Ptr(idEc1),
 		PowerSupplyType:         util.Ptr(model.ElectricalConnectionVoltageTypeTypeAc),
 		PositiveEnergyDirection: util.Ptr(model.EnergyDirectionTypeConsume),
 	})
@@ -254,4 +262,152 @@ func (e *MGCP) AddFeatures() {
 		panic(err)
 	}
 
+	idP1 := electricalConnection.AddParameterDescription(model.ElectricalConnectionParameterDescriptionDataType{
+		ElectricalConnectionId:  util.Ptr(idEc1),
+		MeasurementId:           e.idM1,
+		VoltageType:             util.Ptr(model.ElectricalConnectionVoltageTypeTypeAc),
+		AcMeasuredPhases:        util.Ptr(model.ElectricalConnectionPhaseNameTypeAbc),
+		AcMeasuredInReferenceTo: util.Ptr(model.ElectricalConnectionPhaseNameTypeNeutral),
+		AcMeasurementType:       util.Ptr(model.ElectricalConnectionAcMeasurementTypeTypeReal),
+		AcMeasurementVariant:    util.Ptr(model.ElectricalConnectionMeasurandVariantTypeRms),
+	})
+
+	idP2 := electricalConnection.AddParameterDescription(model.ElectricalConnectionParameterDescriptionDataType{
+		ElectricalConnectionId: util.Ptr(idEc1),
+		MeasurementId:          e.idM2,
+		VoltageType:            util.Ptr(model.ElectricalConnectionVoltageTypeTypeAc),
+		AcMeasurementType:      util.Ptr(model.ElectricalConnectionAcMeasurementTypeTypeReal),
+	})
+
+	idP3 := electricalConnection.AddParameterDescription(model.ElectricalConnectionParameterDescriptionDataType{
+		ElectricalConnectionId: util.Ptr(idEc1),
+		MeasurementId:          e.idM3,
+		VoltageType:            util.Ptr(model.ElectricalConnectionVoltageTypeTypeAc),
+		AcMeasurementType:      util.Ptr(model.ElectricalConnectionAcMeasurementTypeTypeReal),
+	})
+
+	idP41 := electricalConnection.AddParameterDescription(model.ElectricalConnectionParameterDescriptionDataType{
+		ElectricalConnectionId: util.Ptr(idEc1),
+		MeasurementId:          e.idM41,
+		VoltageType:            util.Ptr(model.ElectricalConnectionVoltageTypeTypeAc),
+		AcMeasuredPhases:       util.Ptr(model.ElectricalConnectionPhaseNameTypeA),
+		AcMeasurementType:      util.Ptr(model.ElectricalConnectionAcMeasurementTypeTypeReal),
+		AcMeasurementVariant:   util.Ptr(model.ElectricalConnectionMeasurandVariantTypeRms),
+	})
+
+	idP42 := electricalConnection.AddParameterDescription(model.ElectricalConnectionParameterDescriptionDataType{
+		ElectricalConnectionId: util.Ptr(idEc1),
+		MeasurementId:          e.idM42,
+		VoltageType:            util.Ptr(model.ElectricalConnectionVoltageTypeTypeAc),
+		AcMeasuredPhases:       util.Ptr(model.ElectricalConnectionPhaseNameTypeB),
+		AcMeasurementType:      util.Ptr(model.ElectricalConnectionAcMeasurementTypeTypeReal),
+		AcMeasurementVariant:   util.Ptr(model.ElectricalConnectionMeasurandVariantTypeRms),
+	})
+
+	idP43 := electricalConnection.AddParameterDescription(model.ElectricalConnectionParameterDescriptionDataType{
+		ElectricalConnectionId: util.Ptr(idEc1),
+		MeasurementId:          e.idM43,
+		VoltageType:            util.Ptr(model.ElectricalConnectionVoltageTypeTypeAc),
+		AcMeasuredPhases:       util.Ptr(model.ElectricalConnectionPhaseNameTypeC),
+		AcMeasurementType:      util.Ptr(model.ElectricalConnectionAcMeasurementTypeTypeReal),
+		AcMeasurementVariant:   util.Ptr(model.ElectricalConnectionMeasurandVariantTypeRms),
+	})
+
+	idP51 := electricalConnection.AddParameterDescription(model.ElectricalConnectionParameterDescriptionDataType{
+		ElectricalConnectionId:  util.Ptr(idEc1),
+		MeasurementId:           e.idM51,
+		VoltageType:             util.Ptr(model.ElectricalConnectionVoltageTypeTypeAc),
+		AcMeasuredPhases:        util.Ptr(model.ElectricalConnectionPhaseNameTypeA),
+		AcMeasuredInReferenceTo: util.Ptr(model.ElectricalConnectionPhaseNameTypeNeutral),
+		AcMeasurementType:       util.Ptr(model.ElectricalConnectionAcMeasurementTypeTypeApparent),
+		AcMeasurementVariant:    util.Ptr(model.ElectricalConnectionMeasurandVariantTypeRms),
+	})
+
+	idP52 := electricalConnection.AddParameterDescription(model.ElectricalConnectionParameterDescriptionDataType{
+		ElectricalConnectionId:  util.Ptr(idEc1),
+		MeasurementId:           e.idM52,
+		VoltageType:             util.Ptr(model.ElectricalConnectionVoltageTypeTypeAc),
+		AcMeasuredPhases:        util.Ptr(model.ElectricalConnectionPhaseNameTypeB),
+		AcMeasuredInReferenceTo: util.Ptr(model.ElectricalConnectionPhaseNameTypeNeutral),
+		AcMeasurementType:       util.Ptr(model.ElectricalConnectionAcMeasurementTypeTypeApparent),
+		AcMeasurementVariant:    util.Ptr(model.ElectricalConnectionMeasurandVariantTypeRms),
+	})
+
+	idP53 := electricalConnection.AddParameterDescription(model.ElectricalConnectionParameterDescriptionDataType{
+		ElectricalConnectionId:  util.Ptr(idEc1),
+		MeasurementId:           e.idM53,
+		VoltageType:             util.Ptr(model.ElectricalConnectionVoltageTypeTypeAc),
+		AcMeasuredPhases:        util.Ptr(model.ElectricalConnectionPhaseNameTypeC),
+		AcMeasuredInReferenceTo: util.Ptr(model.ElectricalConnectionPhaseNameTypeNeutral),
+		AcMeasurementType:       util.Ptr(model.ElectricalConnectionAcMeasurementTypeTypeApparent),
+		AcMeasurementVariant:    util.Ptr(model.ElectricalConnectionMeasurandVariantTypeRms),
+	})
+
+	idP54 := electricalConnection.AddParameterDescription(model.ElectricalConnectionParameterDescriptionDataType{
+		ElectricalConnectionId:  util.Ptr(idEc1),
+		MeasurementId:           e.idM54,
+		VoltageType:             util.Ptr(model.ElectricalConnectionVoltageTypeTypeAc),
+		AcMeasuredPhases:        util.Ptr(model.ElectricalConnectionPhaseNameTypeA),
+		AcMeasuredInReferenceTo: util.Ptr(model.ElectricalConnectionPhaseNameTypeB),
+		AcMeasurementType:       util.Ptr(model.ElectricalConnectionAcMeasurementTypeTypeApparent),
+		AcMeasurementVariant:    util.Ptr(model.ElectricalConnectionMeasurandVariantTypeRms),
+	})
+
+	idP55 := electricalConnection.AddParameterDescription(model.ElectricalConnectionParameterDescriptionDataType{
+		ElectricalConnectionId:  util.Ptr(idEc1),
+		MeasurementId:           e.idM55,
+		VoltageType:             util.Ptr(model.ElectricalConnectionVoltageTypeTypeAc),
+		AcMeasuredPhases:        util.Ptr(model.ElectricalConnectionPhaseNameTypeB),
+		AcMeasuredInReferenceTo: util.Ptr(model.ElectricalConnectionPhaseNameTypeC),
+		AcMeasurementType:       util.Ptr(model.ElectricalConnectionAcMeasurementTypeTypeApparent),
+		AcMeasurementVariant:    util.Ptr(model.ElectricalConnectionMeasurandVariantTypeRms),
+	})
+
+	idP56 := electricalConnection.AddParameterDescription(model.ElectricalConnectionParameterDescriptionDataType{
+		ElectricalConnectionId:  util.Ptr(idEc1),
+		MeasurementId:           e.idM56,
+		VoltageType:             util.Ptr(model.ElectricalConnectionVoltageTypeTypeAc),
+		AcMeasuredPhases:        util.Ptr(model.ElectricalConnectionPhaseNameTypeC),
+		AcMeasuredInReferenceTo: util.Ptr(model.ElectricalConnectionPhaseNameTypeA),
+		AcMeasurementType:       util.Ptr(model.ElectricalConnectionAcMeasurementTypeTypeApparent),
+		AcMeasurementVariant:    util.Ptr(model.ElectricalConnectionMeasurandVariantTypeRms),
+	})
+
+	idP6 := electricalConnection.AddParameterDescription(model.ElectricalConnectionParameterDescriptionDataType{
+		ElectricalConnectionId: util.Ptr(idEc1),
+		MeasurementId:          e.idM6,
+		VoltageType:            util.Ptr(model.ElectricalConnectionVoltageTypeTypeAc),
+	})
+
+	if idP1 == nil || idP2 == nil || idP3 == nil || idP41 == nil || idP42 == nil || idP43 == nil || idP51 == nil ||
+		idP52 == nil || idP53 == nil || idP54 == nil || idP55 == nil || idP56 == nil || idP6 == nil {
+		panic("failed to add electrical connection parameter description")
+	}
+
+	for _, m := range []*model.MeasurementIdType{
+		e.idM1,
+		e.idM2,
+		e.idM3,
+		e.idM41,
+		e.idM42,
+		e.idM43,
+		e.idM51,
+		e.idM52,
+		e.idM53,
+		e.idM54,
+		e.idM55,
+		e.idM56,
+		e.idM6,
+	} {
+		e.setMeasurementForId(m, 0.0)
+	}
+}
+
+func (e *MGCP) setMeasurementForId(id *model.MeasurementIdType, value float64) {
+	// TODO
+}
+
+func (e *MGCP) getMeasurementForId(id *model.MeasurementIdType) float64 {
+	// TODO
+	return 0.0
 }
