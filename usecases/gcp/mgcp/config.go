5b4eb1577dde45ab2f31d55fa3cf72d318aca3dc
Nils Prommersberger <nils.prommersberger@icloud.com>
2024-10-16 08:20:22 +0200

    Create config types

---
+++ usecases/gcp/mgcp/config.go
@@ -0,0 +1,22 @@
+package mgcp
+
+import "github.com/enbility/spine-go/model"
+
+type MonitorPvFeedInPowerLimitationFactorConfig struct {
+}
+
+type MonitorPowerConfig struct {
+	ValueConstraints *model.MeasurementConstraintsDataType
+}
+
+type MonitorEnergyConfig struct {
+}
+
+type MonitorCurrentConfig struct {
+}
+
+type MonitorVoltageConfig struct {
+}
+
+type MonitorFrequencyConfig struct {
+}
